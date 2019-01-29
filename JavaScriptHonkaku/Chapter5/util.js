b77e415c5dfbd471f2b66173e0e6517cf8981ffc
yokkun <yoshi.sano@gmail.com>
2019-01-29 13:45:25 +0900

    20190128class

--- JavaScriptHonkaku/Chapter5/util.js
+++ JavaScriptHonkaku/Chapter5/util.js
@@ -1,26 +1,27 @@
 <script>
-export class Member { //class_propをモジュール化した例
+//class_propをモジュール化した例
+export class Member{
 	//コンストラクタ
-		constructor(firstName, lastName){
-			this.firstName = firstName;
-			this.lastName = lastName;
-		}
-		//firstNameプロパティ
-		get firstName(){
-			return this._firstName; //privateをあらわす_をつけている。thisはクロージャの仕組みを利用したグローバル変数
-		}
-		set firstName(value){
-			this._firstName = value;
-		}
-		//lastNameプロパティ
-		get lastName(){
-			return this._lastName;
-		}
-		set lastName(value){
-			this._lastName = value;
-		}
-		getName(){
-			return this.lastName + this.firstName;
-		}
+	constructor(firstName, lastName){
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
+	//firstNameプロパティ
+	get firstName(){
+		return this._firstName; //privateをあらわす_をつけている。thisはクロージャの仕組みを利用したグローバル変数
+	}
+	set firstName(value){
+		this._firstName = value;
+	}
+	//lastNameプロパティ
+	get lastName(){
+		return this._lastName;
+	}
+	set lastName(value){
+		this._lastName = value;
+	}
+	getName(){
+		return this.lastName + this.firstName;
+	}
+}
 </script>
